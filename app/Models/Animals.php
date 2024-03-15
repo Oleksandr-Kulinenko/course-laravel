51b9b3ab767823e2ff9bbdf974ab4a6594d73b38
Oleksandr Kulinenko <sanyakulinenko@ukr.net>
2024-03-15 10:51:51 +0200

    add new connection in models

--- app/Models/Animals.php
+++ app/Models/Animals.php
@@ -4,11 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Animals extends Model
 {
     public function feeds_animal(): HasMany
     {
         return $this->hasMany(FeedsAnimals::class);
+    }
+
+    public function get_animal(): BelongsTo
+    {
+        return $this->BelongsTo(Employees::class,'employee_id');
+    }
+
+    public function get_feed(): BelongsTo
+    {
+        return $this->BelongsTo(Feeds::class,'feed_id');
     }
 }
--- app/Models/Employees.php
+++ app/Models/Employees.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Employees extends Model
 {
     public function animals_employee(): HasMany
     {
         return $this->hasMany(EmployeesAnimals::class);
+    }
+
+    public function get_animal(): BelongsTo
+    {
+        return $this->BelongsTo(Animals::class,'animal_id');
     }
 }
